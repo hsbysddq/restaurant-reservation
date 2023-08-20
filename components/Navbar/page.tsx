7f10bdb8c35f86af74ad69f07e56ecc13fbcc2d7
hsbysddq <hsby15sddq@gmail.com>
2023-08-20 15:54:55 +0700

    Create Navbar

---
+++ components/Navbar/page.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+React
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <p>Navbar</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>Menu Makanan</li>
+          <li>Reservasi</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
