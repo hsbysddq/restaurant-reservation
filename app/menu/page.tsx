3845ed300b2b00241637c658ce542d85cb3bf455
hsbysddq <hsby15sddq@gmail.com>
2023-08-25 16:58:13 +0700

    Edit menu section

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,22 @@
-import React from "react"
-
+import Image from "next/image"
 const Menu = () => {
-  return <div>Menu</div>
+  return (
+    <div className="">
+      <div className="flex">
+        <div className="basis-4/12">
+          <Image src="/menu1.svg" width={555} height={652} alt="Menu" />
+        </div>
+
+        <div className="basis-4/12">
+          <Image src="/menu1.svg" width={555} height={652} alt="Menu" />
+        </div>
+
+        <div className="basis-4/12">
+          <Image src="/menu1.svg" width={555} height={652} alt="Menu" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Menu
